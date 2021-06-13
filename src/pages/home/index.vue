41828b8094f1edb28dfb7ad919e21074b76f9d5a
unknown <1033640891@qq.com>
2021-06-13 20:52:46 +0800

    6.13

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,64 @@
 <template>
-  <view>首页</view>
+  <view>
+     <view>
+       <uni-segmented-control
+        :current="current"
+        :values="items.map(v=>v.title)"
+        @clickItem="onClickItem"
+        style-type="text"
+        active-color="#d4237a"
+       ></uni-segmented-control>
+       <view class="content">
+        <view v-if="current === 0">
+           <home-recommend></home-recommend>
+        </view>
+        <view v-if="current === 1">
+           <home-category></home-category>
+        </view>
+        <view v-if="current === 2">
+           <home-new></home-new>
+        </view>
+        <view v-if="current === 3">
+           <home-album></home-album>
+        </view>
+       </view>
+     </view>
+  </view>
 </template>
 
-<script>
-export default {
 
+<script>
+ import homeAlbum from "./home-album";
+ import homeCategory from "./home-category";
+ import homeNew from "./home-new";
+ import homeRecommend from "./home-recommend";
+ import {uniSegmentedControl} from '@dcloudio/uni-ui'
+export default {
+  components:{
+    homeAlbum,
+    homeCategory,
+    homeNew,
+    homeRecommend,
+    uniSegmentedControl
+  },
+  data(){
+    return{
+      items:[
+        {title:"推荐"},
+        {title:"分类"},
+        {title:"最新"},
+        {title:"专辑"}
+      ],
+      current: 0
+    };
+  },
+  methods:{
+    onClickItem(e){
+      if(this.current !== e.currentIndex){
+          this.current = e.currentIndex; 
+      }
+    }
+  }
 }
 </script>
 
